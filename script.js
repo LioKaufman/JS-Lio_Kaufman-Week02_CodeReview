0a085ac58affb843124f39fa900974ace5f0dc3b
Lio Kaufman <lio.kaufman@gmail.com>
2017-02-24 15:55:57 +0100

    stylistic changes

--- script.js
+++ script.js
@@ -22,16 +22,19 @@ function dieRoll() {
 };
 
 
-function playGame(playerID) {
+function playGame(die1, die2, die3) {
 
 /* 
 	This function uses an funciton to create an array of 3 random 
 	die rolls and places them at the outcome of each player, 
 	based on their player ID 
 */
- 
+
 	var outcome = [dieRoll(), dieRoll(), dieRoll()];
-	playerID.innerText = outcome[0] + " & " + outcome[1] + " & " + outcome[2];	
+
+	die1.innerHTML = outcome[0];
+	die2.innerHTML = outcome[1];
+	die3.innerHTML = outcome[2];
 };
 
 
